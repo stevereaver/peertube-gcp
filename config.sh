b684d6b6e72917d4a7982658f2bcecd1a1baa682
Stephen Bancroft <stephen.bancroft@kasna.com.au>
2022-01-10 14:12:59 +1100

    First commit

---
+++ config.sh
@@ -0,0 +1,5 @@
+# Project Constants
+export REGION="australia-southeast1"
+export LOCATION="australia-southeast1"
+export ZONE="australia-southeast1-a"
+export TERRAFORM_STATE_BUCKET="terraform-state"
